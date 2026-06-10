bf8d9f2378b1df2d1851c62e9d85ed5f57fcd829
Axel <axelcastelot@gmail.com>
2026-06-10 09:28:59 +0200

    Feat: Ajout des pages

--- application-meteo/app/_layout.tsx
+++ application-meteo/app/_layout.tsx
@@ -1,11 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider, Stack } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      
+      <Stack screenOptions={{headerShown: false}}>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="details/[id]" options={{ headerShown: false }} />
+      </Stack>
     </ThemeProvider>
   );
 }
